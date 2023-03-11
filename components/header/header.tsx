90f4df2acdf2b150c7f0dedbca6931c00646fc64
Per Halvor Tryggeseth <per.halvor@gmail.com>
2023-03-11 19:25:02 +0100

    escape apostrophe in header

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link'
 
 export const Header: React.FC = () => {
-  return <header><Link href="/">Runner's caclulator</Link></header>;
+  return <header><Link href="/">Runner&apos;s caclulator</Link></header>;
 };
