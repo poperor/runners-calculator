7807f3d740f5057f968286c6aa41d159d506bd64
Per Halvor Tryggeseth <per.halvor@gmail.com>
2023-03-12 17:50:45 +0100

    better styling and handling of enter in minutes per km input

--- components/inputs/minutes-per-km-input.tsx
+++ components/inputs/minutes-per-km-input.tsx
@@ -1,17 +1,18 @@
-import { useState, useContext } from "react";
+import { useState, useContext, KeyboardEvent } from "react";
 import {
   fromMinutesPerKm,
   MinutesPerKm,
-  toMinutesPerKm
+  toMinutesPerKm,
 } from "../../lib/conversion";
-import styles from './input.module.css'
-import utilityStyles from '../../styles/utility.module.css'
+import styles from "./input.module.css";
+import utilityStyles from "../../styles/utility.module.css";
 import { CanonicalKph } from "../../context/canonical-kph";
-
 
 export const MinutesPerKmInput = () => {
   const { canonicalKph, setCanonicalKph } = useContext(CanonicalKph);
-  const [minutesPerKm, setMinutesPerKm] = useState<MinutesPerKm>(toMinutesPerKm(canonicalKph) || {min: 0, sec: 0});
+  const [minutesPerKm, setMinutesPerKm] = useState<MinutesPerKm>(
+    toMinutesPerKm(canonicalKph) || { min: 0, sec: 0 }
+  );
 
   const onMinChange = (event: React.FormEvent<HTMLInputElement>): void => {
     const min = event.currentTarget.valueAsNumber;
@@ -27,12 +28,40 @@ export const MinutesPerKmInput = () => {
     setCanonicalKph(fromMinutesPerKm(newMinPerKm));
   };
 
+  const handleKeyboardEvent = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      console.log('do validate');
+      e.currentTarget.blur()
+    }
+  };
+
   return (
     <div className={utilityStyles.verticalAlign}>
-      <input className={styles.mininput} type="number" onChange={onMinChange} value={minutesPerKm.min || ''} />{" "}
-      min{" "}
-      <input className={styles.secinput} type="number" onChange={onSecChange} value={minutesPerKm.sec || ''} />{" "}
-      sec
+      <div className={styles.timeinputbox}>
+        <label className={styles.timelabel} htmlFor="minutes">
+          min
+        </label>
+        <input
+          id="minutes"
+          className={styles.timeinput}
+          type="number"
+          onChange={onMinChange}
+          value={minutesPerKm.min || ""}
+        />{" "}
+      </div>
+      <div className={styles.secinputbox}>
+        <label className={styles.timelabel} htmlFor="seconds">
+          sec
+        </label>
+        <input
+          id="seconds"
+          className={styles.timeinput}
+          type="number"
+          onChange={onSecChange}
+          onKeyDown={handleKeyboardEvent}
+          value={minutesPerKm.sec || ""}
+        />
+      </div>
     </div>
   );
 };
