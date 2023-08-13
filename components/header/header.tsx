926f5713aec5999a409a08256e00ae2a29049ac8
Per Halvor Tryggeseth <per.halvor@gmail.com>
2023-08-13 13:05:41 +0200

    further css improvements

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -4,9 +4,13 @@ import styles from "./header.module.css";
 export const Header: React.FC = () => {
   return (
     <header className={styles.header}>
-        <div className={styles.homeLink}>
+        <div className={styles.logo}>
           <Link href="/">Runner&apos;s caclulator</Link>
         </div>
+        <nav className={styles.nav}>
+          <Link className={styles.homeLink} href="/">home</Link>
+          <Link className={styles.aboutLink} href="/">about</Link>
+        </nav>
     </header>
   );
 };
